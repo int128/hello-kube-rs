b9db28c706b00183cc9b11e73bd8e6b8524b36ae
Hidetake Iwata <int128@gmail.com>
2022-11-06 16:48:55 +0900

    Show pods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use k8s_openapi::api::core::v1::Pod;
+use kube::{Api, Client, ResourceExt};
+use kube::api::ListParams;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::default_namespaced(client);
+
+    for pod in pods.list(&ListParams::default().limit(10)).await? {
+        println!("Pod {}", pod.name_any());
+    }
+
+    Ok(())
 }
